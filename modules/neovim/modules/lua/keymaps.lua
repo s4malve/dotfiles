702259902959067f8f1a1ec210d5ee72051d8cac
s4malve <42875378+s4malve@users.noreply.github.com>
2025-08-01 16:24:32 -0500

    feat: improve nvim config

--- modules/neovim/modules/lua/keymaps.lua
+++ modules/neovim/modules/lua/keymaps.lua
@@ -1,8 +1,10 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 vim.opt.colorcolumn = "80"
 vim.opt.cursorline = true
+
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
